9b193a972ffd2a185b749fd22be013ee1d0bec4e
Jared Wheeler <>
2018-11-02 12:39:41 -0500

    Added XCTest code for ISO8601 Date Formatter

--- CasechekTestTests/CasechekTestTests.swift
+++ CasechekTestTests/CasechekTestTests.swift
@@ -19,8 +19,24 @@ class CasechekTestTests: XCTestCase {
         
     }
 
-    func testExample() {
-        
+    func testISO8601DateFormatter() {
+        let y = "2018"
+        let mo = "10"
+        let d = "01"
+        let h = "14"
+        let mi = "05"
+        let s = "57"
+        let ms = "017"
+        let dateString = y+"-"+mo+"-"+d+"T"+h+":"+mi+":"+s+"."+ms
+        let date = Formatter.dateFrom(IS08601String: dateString)
+        let calendar = Calendar.current
+        assert(date != nil)
+        assert(calendar.component(.year, from: date!) == Int(y))
+        assert(calendar.component(.month, from: date!) == Int(mo))
+        assert(calendar.component(.day, from: date!) == Int(d))
+        assert(calendar.component(.hour, from: date!) == Int(h))
+        assert(calendar.component(.minute, from: date!) == Int(mi))
+        assert(calendar.component(.second, from: date!) == Int(s))
     }
 
     func testPerformanceExample() {
